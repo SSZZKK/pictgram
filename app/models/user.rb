50726c8787560ed5fca6062a034c37f2ee67cef9
SSZZKK <michael.suzuki@aol.com>
2021-06-25 02:21:50 +0000

    0000

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+ 
+  has_secure_password
+  validates :password, presence: true, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,32}/ } 
 end
