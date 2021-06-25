cc2609f365d2a89576214119d449803e3861483b
SSZZKK <michael.suzuki@aol.com>
2021-06-25 02:41:39 +0000

    0001

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to root_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
-      render :new
+           render :new
     end
   end
 
@@ -17,11 +18,11 @@ class SessionsController < ApplicationController
     redirect_to root_url, info: 'ログアウトしました'
   end
 
-   private
+  private
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   def log_out
     session.delete(:user_id)
     @current_user = nil
@@ -30,6 +31,6 @@ class SessionsController < ApplicationController
   def session_params
     params.require(:session).permit(:email, :password)
   end
-end
 
   
+end
