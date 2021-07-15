8e183eace674e0a5e06c02e7bc5cf1a08d4a9af0
SSZZKK <michael.suzuki@aol.com>
2021-07-15 20:27:07 +0000

    コメント

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    @topics = Topic.all.includes(:favorite_users)
   end
 
   def new
@@ -9,11 +9,11 @@ class TopicsController < ApplicationController
 
   def create
     @topic = current_user.topics.new(topic_params)
-    
+    # binding.pry 
     if @topic.save
       redirect_to topics_path, success: '投稿に成功しました'
     else
-      flash.now[:danger] = "投稿に失敗しました"
+      flash.now[:danger] = '投稿に失敗しました'
       render :new
     end
   end
