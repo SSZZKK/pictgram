6c0d52813d59f3fd8df02c46fbe4b8b0d5d08a19
SSZZKK <michael.suzuki@aol.com>
2021-07-19 07:55:02 +0000

    20210720

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  
+  def new
+    
+    @topic_id = params[:topic_id]
+    @comment = Comment.new
+  end
+    
+  if @comment.save
+      redirect_to topics_path, success: 'コメント登録に成功しました'
+  else
+      flash.now[:danger] = "コメント登録に失敗しました"
+      render :new
+  end
+end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :topic_id)
+  end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :topic
+ 
+  validates :user_id, presence: true
+  validates :text, presence: true
+end
