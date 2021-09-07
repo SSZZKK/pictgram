5a28d173703dab97cf0a97ccd29559616bbe0426
SSZZKK <michael.suzuki@aol.com>
2021-09-07 19:30:27 +0000

    S3設定

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+CarrierWave.configure do |config|
+  config.storage :fogs
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'sszzkk' #ENV['AWS_BUCKET']
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAUGPYIMIXJOZMKL7F' ,#ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: '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' , #ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'p-northeast-1' , #ENV['AWS_DEFAULT_REGION'],
+    path_style: true
+  }
+end
